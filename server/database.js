a42863b04684f6eb9c0fa651bf3e1cee5f768c23
Daniel Maximov <139768935+DanielMaximov1995@users.noreply.github.com>
2023-07-26 13:46:01 +0300

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://CinemaD-B:<password>@cinemadb.k23qi.mongodb.net/?retryWrites=true&w=majority').then(() => {
+mongoose.connect(process.env.MongoDB).then(() => {
     console.log("MongoDB is connected");
 }).catch((err) => {
     console.log(`MongoDB not Connected ${err}`);
